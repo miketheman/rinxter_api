8af381af5a2657c62a7a4a67a2999c39626a1927
Mike Fiedler <miketheman@gmail.com>
2014-03-23 22:25:59 -0400

    minor refactor of vcr invocation

--- spec/lib/rinxter_api/skater_spec.rb
+++ spec/lib/rinxter_api/skater_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
-describe 'RinxterApi::Skater' do
+describe 'RinxterApi::Skater', :vcr do
   before(:all) do
     @skater = RinxterApi::Skater.new
   end
 
-  describe '#get_by_id', :vcr do
+  describe '#get_by_id' do
     it 'retrieves Skater based on ID' do
       response = @skater.get_by_id(35)
       expect(response.name).to eq 'Suzy Hotrod'
     end
   end
 
-  describe '#find_by_name', :vcr do
+  describe '#find_by_name' do
     it 'retrieves Skater based on name' do
       response = @skater.find_by_name(league_id: 1, name: 'Suzy')
       expect(response.name).to eq 'Suzy Hotrod'
